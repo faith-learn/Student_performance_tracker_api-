4712f478976227054dee466473eebb650a92bdd0
Faith Awuor <faithhope264@gmail.com>
2025-12-22 10:52:29 +0300

    Registered  profile and core models in admin panel

--- student_tracker/student_tracker_app/admin.py
+++ student_tracker/student_tracker_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Unit, Assessment, PerformanceStatus, Reminder
+from .models import Profile, Unit, Assessment, Reminder
 
+admin.site.register(Profile)
 admin.site.register(Unit)
 admin.site.register(Assessment)
-admin.site.register(PerformanceStatus)
 admin.site.register(Reminder)
