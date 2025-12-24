a7bae415580283f186500f31dba71582fc8d5b86
Faith Awuor <faithhope264@gmail.com>
2025-12-24 10:15:26 +0300

    Created  serializers for performance tracking and reminders

--- student_tracker/student_tracker_app/serializers.py
+++ student_tracker/student_tracker_app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Unit, Assessment, PerformanceStatus, Reminder
+from .models import Unit, Assessment, Reminder, Profile
 
 class UnitSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,28 +10,13 @@ class AssessmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Assessment
         fields = '__all__'
-        read_only_fields = ('student',)
-
-    def create(self, validated_data):
-        request = self.context.get('request')
-        validated_data['student'] = request.user
-        return super().create(validated_data)
-
-# --- ADD THESE NEW CLASSES BELOW ---
 
 class ReminderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reminder
         fields = '__all__'
-        read_only_fields = ('student',) # Ensures users can't "assign" reminders to others
 
-    def create(self, validated_data):
-        # Automatically sets the student to the logged-in user
-        request = self.context.get('request')
-        validated_data['student'] = request.user
-        return super().create(validated_data)
-
-class PerformanceStatusSerializer(serializers.ModelSerializer):
+class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
-        model = PerformanceStatus
+        model = Profile
         fields = '__all__'
