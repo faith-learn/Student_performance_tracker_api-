e1ccf18aa647abf0f93c2412d2c7a749e89fc446
Faith Awuor <faithhope264@gmail.com>
2025-12-26 15:45:32 +0300

    Added  Reminder API and basic performance status logic

--- student_tracker/student_tracker_app/views.py
+++ student_tracker/student_tracker_app/views.py
@@ -1,11 +1,14 @@
 from rest_framework import viewsets, permissions
+from django.contrib.auth.models import User
 from .models import Unit, Assessment, Reminder
 from .serializers import UnitSerializer, AssessmentSerializer, ReminderSerializer
+
 
 class UnitViewSet(viewsets.ModelViewSet):
     queryset = Unit.objects.all()
     serializer_class = UnitSerializer
     permission_classes = [permissions.IsAuthenticated]
+
 
 class AssessmentViewSet(viewsets.ModelViewSet):
     serializer_class = AssessmentSerializer
@@ -13,9 +16,23 @@ class AssessmentViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         user = self.request.user
-        if user.profile.role == 'lecturer':
+        if user.is_staff:
             return Assessment.objects.all()
         return Assessment.objects.filter(student=user)
+
+    def perform_create(self, serializer):
+        """
+        Automatically assign performance status
+        """
+        marks = serializer.validated_data.get("marks")
+
+        if marks >= 50:
+            status = "Pass"
+        else:
+            status = "Fail"
+
+        serializer.save(student=self.request.user, status=status)
+
 
 class ReminderViewSet(viewsets.ModelViewSet):
     serializer_class = ReminderSerializer
@@ -23,3 +40,6 @@ class ReminderViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return Reminder.objects.filter(student=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(student=self.request.user)
