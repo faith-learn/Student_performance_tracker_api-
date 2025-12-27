b5cc2e4c8dbe3c191e87b0e26b2d641c84b5fb3e
Faith Awuor <faithhope264@gmail.com>
2025-12-27 09:52:11 +0300

    Integrated  AI performance logic and validation into serializers

--- student_tracker/student_tracker_app/serializers.py
+++ student_tracker/student_tracker_app/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import Unit, Assessment, Reminder
+from .utils import calculate_performance_status
 
 
 class UnitSerializer(serializers.ModelSerializer):
@@ -12,6 +14,16 @@ class AssessmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Assessment
         fields = "__all__"
+
+    def validate_marks(self, value):
+        if value < 0 or value > 100:
+            raise serializers.ValidationError("Marks must be between 0 and 100.")
+        return value
+
+    def create(self, validated_data):
+        marks = validated_data.get("marks")
+        validated_data["status"] = calculate_performance_status(marks)
+        return super().create(validated_data)
 
 
 class ReminderSerializer(serializers.ModelSerializer):
